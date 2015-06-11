18fbff649eb2b6c54df68d9bbed0064cf9b66822
Medtouch Software Médico <Medtouch@Air-de-Medtouch.home>
2015-06-11 19:46:01 -0300

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by Medtouch Software Médico on 11/06/15.
+//  Copyright (c) 2015 Medtouch Software Médico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
